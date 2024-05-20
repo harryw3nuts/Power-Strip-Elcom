5b96be621c8efe7f7db98f16597a7d6e836d293e
pritw3nuts <prit.w3nuts@gmail.com>
2024-05-20 08:59:28 +0530

    text color change animation done

--- src/components/ScrollTextColor.jsx
+++ src/components/ScrollTextColor.jsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
-import PsImage from '@/asset/images/ps-text-image.png'
+import PsImage from "@/asset/images/ps-text-image.png";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
 
-const ScrollTextColor = ()=> {
-
+const ScrollTextColor = () => {
     gsap.registerPlugin(ScrollTrigger);
 
     const pinRef = useRef(null);
@@ -17,139 +16,199 @@ const ScrollTextColor = ()=> {
     const rightTextRef = useRef(null);
     const outerLeftTextRef = useRef(null);
     const outerRightTextRef = useRef(null);
+    const [imageLoaded, setImageLoaded] = useState(false);
+    const [imageClipperDimensions, setImageClipperDimensions] = useState({
+        width: 0,
+        height: 0,
+    });
 
-    useEffect(()=>{
-            
-    },[]);
+    useEffect(() => {
+        // const handleResize = () => {
+        setImageClipperDimensions({
+            width: imageClipperRef.current.getBoundingClientRect().width,
+            height: imageClipperRef.current.getBoundingClientRect().height,
+        });
+        // };
+        // window.addEventListener("resize", handleResize);
 
+        // Initial load
+        // handleResize();
 
-    useEffect(()=>{
+        // Remove event listener on cleanup
+        // return () => {
+        //     window.removeEventListener("resize", handleResize);
+        // };
+    }, [imageLoaded]);
 
-            const animationHandler = () => {
+    useEffect(() => {
+        if (!imageLoaded) return;
 
-                const scaleImg = 0.465;
-                const textDivWidth = imageClipperRef.current.getBoundingClientRect().width
-                const textDivHeight = imageClipperRef.current.getBoundingClientRect().height
-                let initilWidth = ( 1 * textDivWidth ) / scaleImg
-                let initilHeight = ( 1 * textDivHeight ) / scaleImg
+        // const animationHandler = () => {
 
-                // width : "44.44%"
-                // height : "25%"
-                gsap.fromTo(intextRef.current, {  width: textDivWidth, height: textDivHeight }, {
-                    width: initilWidth,
-                    height: initilHeight,
-                    scrollTrigger: {
-                        trigger: pinRef.current,
-                        start: 'top top',
-                        end: '200% bottom',
-                        scrub: 0.2,
-                        onEnterBack: () => {
+        const scaleImg = 0.465;
+        const textDivWidth = imageClipperDimensions.width * scaleImg;
+        const textDivHeight = imageClipperDimensions.height * scaleImg;
+        console.log("textDivWidth", textDivWidth);
+        console.log("imageClipperDimensions.width", imageClipperDimensions.width);
 
-                        },
-                    }
-                })
-                //scale: 0.465
-                gsap.fromTo(imageClipperRef.current, { scale: 0.465 }, {
-                    scale: 1,
-                    scrollTrigger: {
-                        trigger: pinRef.current,
-                        start: 'top top',
-                        end: '200% bottom',
-                        scrub: 0.2,   
-                    }
-                })
-                console.log(textDivWidth);
-
-                gsap.fromTo(outerLeftTextRef.current, { x: 0 }, {
-                    x: 250,
-                    scrollTrigger: {
-                        trigger: pinRef.current,
-                        start: 'top top',
-                        end: '200% bottom',
-                        scrub: 0.5,   
-                    }
-                })
-
-                gsap.fromTo(leftTextRef.current, { x: 0 }, {
-                    x: 250,
-                    scrollTrigger: {
-                        trigger: pinRef.current,
-                        start: 'top top',
-                        end: '200% bottom',
-                        scrub: 0.5,
-                    }
-                })
-
-                gsap.fromTo(outerRightTextRef.current, { x: 0 }, {
-                    x: -250,
-                    scrollTrigger: {
-                        trigger: pinRef.current,
-                        start: 'top top',
-                        end: '200% bottom',
-                        scrub: 0.5,   
-                    }
-                })
-
-                gsap.fromTo(rightTextRef.current, { x: 0 }, {
-                    x: -250,
-                    scrollTrigger: {
-                        trigger: pinRef.current,
-                        start: 'top top',
-                        end: '200% bottom',
-                        scrub: 0.5,
-                    }
-                })
-                //console.log({scaleImg, textDivHeight, textDivWidth, initilHeight, initilWidth});
-            }
-
-            animationHandler()
-
-            // PinSec
-            const pinHandler = () => {
-                const pin = gsap.to(pinRef.current, {
-                    scrollTrigger: {
-                        trigger: pinRef.current,
-                        start: "top top",
-                        end: '200% bottom',
-                        pin: true,
-                        markers: true
+        gsap.fromTo(
+            intextRef.current,
+            { width: textDivWidth, height: textDivHeight },
+            {
+                width: imageClipperDimensions.width,
+                height: imageClipperDimensions.height,
+                scrollTrigger: {
+                    trigger: pinRef.current,
+                    start: "top top",
+                    end: "200% bottom",
+                    scrub: 0.5,
+                    onUpdate: (self) => {
+                        // console.log('self', self);
+                        // console.log('imageClipperDimensions.width', imageClipperDimensions.width);
+                        // const progress = self.progress;
+                        // const newWidth = textDivWidth * (1 - progress) + scaleImg * textDivWidth * progress;
+                        // const newHeight = textDivHeight * (1 - progress) + scaleImg * textDivHeight * progress;
+                        // gsap.set(intextRef.current, { width: newWidth, height: newHeight });
                     },
-              });
+                },
             }
+        );
 
-            const pinDelayTime = 2000;
-            const pinHandlerTimeout = setTimeout(pinHandler, pinDelayTime);
+        gsap.fromTo(
+            imageClipperRef.current,
+            { scale: 0.465 },
+            {
+                scale: 1,
+                scrollTrigger: {
+                    trigger: pinRef.current,
+                    start: "top top",
+                    end: "200% bottom",
+                    scrub: 0.5,
+                },
+            }
+        );
 
-            return () => {
-                clearTimeout(pinHandlerTimeout);
-            };
-    },[])
+        gsap.fromTo(
+            outerLeftTextRef.current,
+            { x: 0 },
+            {
+                x: 250,
+                scrollTrigger: {
+                    trigger: pinRef.current,
+                    start: "top top",
+                    end: "200% bottom",
+                    scrub: 0.5,
+                },
+            }
+        );
+
+        gsap.fromTo(
+            leftTextRef.current,
+            { x: 0 },
+            {
+                x: 250,
+                scrollTrigger: {
+                    trigger: pinRef.current,
+                    start: "top top",
+                    end: "200% bottom",
+                    scrub: 0.5,
+                },
+            }
+        );
+
+        gsap.fromTo(
+            outerRightTextRef.current,
+            { x: 0 },
+            {
+                x: -250,
+                scrollTrigger: {
+                    trigger: pinRef.current,
+                    start: "top top",
+                    end: "200% bottom",
+                    scrub: 0.5,
+                },
+            }
+        );
+
+        gsap.fromTo(
+            rightTextRef.current,
+            { x: 0 },
+            {
+                x: -250,
+                scrollTrigger: {
+                    trigger: pinRef.current,
+                    start: "top top",
+                    end: "200% bottom",
+                    scrub: 0.5,
+                },
+            }
+        );
+        //console.log({scaleImg, textDivHeight, textDivWidth, initilHeight, initilWidth});
+        // }
+
+        // animationHandler()
+
+        // PinSec
+        const pinHandler = () => {
+            const pin = gsap.to(pinRef.current, {
+                scrollTrigger: {
+                    trigger: pinRef.current,
+                    start: "top top",
+                    end: "200% bottom",
+                    pin: true,
+                    markers: true,
+                },
+            });
+        };
+
+        const pinDelayTime = 2000;
+        const pinHandlerTimeout = setTimeout(pinHandler, pinDelayTime);
+
+        return () => {
+            clearTimeout(pinHandlerTimeout);
+        };
+    }, [imageLoaded]);
 
     return (
         <div className="zoomer-wrap">
             <div class="zoomer" ref={pinRef}>
-            <div class="text textwrapout">
-                <div class="clipper" ref={textClipperRef}>
-                    <h2 ref={leftTextRef}>Power</h2>
-                    <h2 ref={rightTextRef} className="heading2">Strip</h2>
+                <div class="text textwrapout">
+                    <div class="clipper" ref={textClipperRef}>
+                        <h2 ref={leftTextRef}>Power</h2>
+                        <h2 ref={rightTextRef} className="heading2">
+                            Strip
+                        </h2>
+                    </div>
                 </div>
-            </div>
-            <div class="images">
-                <div class="clipper" ref={imageClipperRef}>
-                    <Image src={PsImage} alt="PsImage" />
+                <div class="images">
+                    <div class="clipper" ref={imageClipperRef}>
+                        <Image
+                            src={PsImage}
+                            alt="PsImage"
+                            onLoad={() => {
+                                setImageClipperDimensions({
+                                    width: imageClipperRef.current.getBoundingClientRect().width,
+                                    height:
+                                        imageClipperRef.current.getBoundingClientRect().height,
+                                });
+                                setImageLoaded(true);
+                            }}
+                        />
+                    </div>
                 </div>
-            </div>
-            <div class="textwrap">
-                <div class="intextwrap" ref={intextRef}>
-                    <div class="inner_intextwrap">
-                        <h2 ref={outerLeftTextRef}>Power</h2>
-                        <h2 ref={outerRightTextRef} className="heading2" >Strip</h2>
+                <div class="textwrap">
+                    <div class="intextwrap" ref={intextRef}>
+                        <div class="inner_intextwrap">
+                            <h2 ref={outerLeftTextRef}>Power</h2>
+                            <h2 ref={outerRightTextRef} className="heading2">
+                                Strip
+                            </h2>
+                        </div>
                     </div>
                 </div>
             </div>
-            </div>
         </div>
-    )
-}
+    );
+};
 
 export default ScrollTextColor;
